c17fbfdeb07fadef25db777f0219a9ce0d1a35a5
Daniel Gohl <danielgohl13@gmail.com>
2020-03-10 11:42:24 -0400

    update config

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-Vue.config.ignoredElements = [
-    'my-custom-web-component',
-    'another-web-component',
-    // Use a `RegExp` to ignore all elements that start with "ion-"
-    // 2.5+ only
-    /^ion-/
-  ]
